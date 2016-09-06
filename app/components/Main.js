d8a56d674c6737bf1600f5f5ab1985da0dbc4a7f
Aravind M <murali.aravind94@gmail.com>
2016-09-06 19:44:20 +0530

    Add final modifications

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import '../main.css';
 
 class Main extends React.Component {
 
@@ -9,7 +11,12 @@ class Main extends React.Component {
   render() {
     return (
       <div className="main-container">
-       {this.props.children}
+      <ReactCSSTransitionGroup
+        transitionName="appear"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}>
+       {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+      </ReactCSSTransitionGroup>
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,14 @@ var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   inject: 'body'
 });
 module.exports = {
+  devtool: 'source-map',
   entry: [
     './app/index.js'
   ],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
   output: {
